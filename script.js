e3ad8f360c3ddf77f62299f47d44db0261b569ff
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-10-11 17:43:02 -0400

    Connected pupil movement to mouse movement with JS

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Input Setup
+// HTML Elements
+const pupilsEl = document.getElementsByClassName('pupil')
+
+// Input Setup - This tracks how far the mouse can move within the window
 let input = {
     mouseX: {
         start: 0,
@@ -15,17 +18,23 @@ let input = {
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
 input.mouseY.range = input.mouseY.end - input.mouseY.start;
 
-// Output Setup
+// Output Setup - This determines how the pupils move. The range is much smaller because I want the pupil elements to move only within the eyeballs
 let output = {
     x: {
-        start: -100,
-        end: 100,
-        current: 0,
+        start: -75,
+        end: 75,
+        current: 0
     },
-    y: {},
+    y: {
+        start: -75,
+        end: 75,
+        current: 0
+    },
 };
 
+// Determine range for x and y axis
 output.x.range = output.x.end - output.x.start;
+output.y.range = output.y.end - output.y.start;
 
 function handleMouseMove(event) {
     //   Mouse X Input
@@ -34,8 +43,15 @@ function handleMouseMove(event) {
     // Mouse Y Input
     input.mouseY.current = event.pageY;
     input.mouseY.fraction = (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
-    //   Calculate how much the X coordinate should change based on the current position of the mouse and add that to the output's starting position to determine how much the output should shift horizontally
+
+    // Calculate how much the X and Y coordinates should change based on the current position of the mouse and add that to the output's starting position to determine how much the output should shift horizontally and vertically
     output.x.current = output.x.start + (input.mouseX.fraction * output.x.range);
+    output.y.current = output.y.start + (input.mouseY.fraction * output.y.range);
+
+    // Apply output to pupil elements for horizontal and vertical movement
+    for (const pupil of pupilsEl) {
+        pupil.style.transform = `translate(${output.x.current}px, ${output.y.current}px)`;
+    }
 
     //   console.log('fraction X', input.mouseX.fraction)
     //   console.log('fraction Y', input.mouseY.fraction)
